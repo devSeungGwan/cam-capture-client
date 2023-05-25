5a9b986394ba308557121496afeb48bf86728c03
송승관 <yanhis97531@gmail.com>
2023-05-25 23:46:56 +0000

    ADD: use cfgnode

---
+++ cam_capture_client/common/__init__.py
@@ -0,0 +1,4 @@
+from .default import _C as cfg
+from .utils import *
+from .logger import Logger
+
---
+++ cam_capture_client/common/default.py
@@ -0,0 +1,20 @@
+from fvcore.common.config import CfgNode as CN
+
+_C = CN()
+_C.CAPTURE = CN()
+_C.CAPTURE.SAVE_WIDTH = 3264
+_C.CAPTURE.SAVE_HEIGHT = 2448
+_C.CAPTURE.CAM_WIDTH = 816
+_C.CAPTURE.CAM_HEIGHT = 612
+_C.CAPTURE.CAM_NO = 0
+_C.CAPTURE.CAM_ROTATE = 0
+_C.CAPTURE.CAM_NAME = "cam"
+_C.CAPTURE.OUTPUT_PATH = "./output"
+
+_C.CONTROL = CN()
+_C.CONTROL.CAPTURE = "c"
+_C.CONTROL.CLOSE = "esc"
+
+_C.LOGGER = CN()
+_C.LOGGER.NAME = "cam-capture"
+_C.LOGGER.CONF_PATH = "./config/logger.json"
