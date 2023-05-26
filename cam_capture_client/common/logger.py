b4dbe5ceb7dd3d90fdbe954ddbef840e03d269f8
송승관 <yanhis97531@gmail.com>
2023-05-26 23:38:11 +0000

    ADD: logger

---
+++ cam_capture_client/common/logger.py
@@ -0,0 +1,26 @@
+import logging
+from fvcore.common.config import CfgNode as CN
+
+from common.utils import read_json
+
+class Logger:
+    def __init__(self, cfg: CN):
+        __logger_conf = read_json(cfg.LOGGER.CONF_PATH)
+        logging.config.dictConfig(__logger_conf)
+
+        self.__logger = logging.getLogger(cfg.LOGGER.NAME)
+    
+    def info(self, msg: str):
+        self.__logger.info(msg)
+        
+    def debug(self, msg: str):
+        self.__logger.debug(msg)
+        
+    def warning(self, msg: str):
+        self.__logger.warning(msg)
+    
+    def error(self, msg: str):
+        self.__logger.error(msg)
+    
+    def critical(self, msg: str):
+        self.__logger.critical(msg)
