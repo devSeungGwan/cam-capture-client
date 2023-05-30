b1474b134d78e80740c116e5b352e12d1e1af327
송승관 <yanhis97531@gmail.com>
2023-05-30 14:24:15 +0000

    ADD: launched on main.py

---
+++ cam_capture_client/main.py
@@ -0,0 +1,14 @@
+from argparse import ArgumentParser
+
+from cam_capture_client import CamCapture
+from cam_capture_client.common import cfg
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    
+    parser.add_argument("--config", type=str, default="./config/config.yaml", help="config file path")
+    args = parser.parse_args()
+    
+    cfg.merge_from_file(args.config)
+    client = CamCapture(cfg)
+    client.run()
